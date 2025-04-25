98b74a536e49d6edb36a556385796ea8dfc33dba
CW0r <charl13worth@gmail.com>
2025-04-25 15:43:01 +0100

    Caesar Cipher

---
+++ Caeser Shift/CaeserCipher.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace Caeser_Shift
+{
+    class CaeserCipher
+    {
+        private readonly List<char> BaseAlphabet = new List<char> { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        public string EncodingShift(string plaintText, int shiftValue)
+        {
+            char[] characters = plaintText.ToCharArray();
+            char[] cipherText = new char[characters.Length];
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                int index = (BaseAlphabet.IndexOf(characters[i]) + shiftValue) % 26;
+                cipherText[i] = BaseAlphabet[index];
+            }
+
+            return CharArrayToString(cipherText);
+        }
+
+        public string DecodingShift(string cipherText, int shiftValue)
+        {
+            char[] characters = cipherText.ToCharArray();
+            char[] plainText = new char[characters.Length];
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                int index = (BaseAlphabet.IndexOf(characters[i]) - shiftValue) % 26;
+                plainText[i] = BaseAlphabet[index];
+            }
+
+            return CharArrayToString(plainText);
+        }
+
+        private string CharArrayToString(char[] charArray)
+        {
+            string returnString = "";
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                returnString += charArray[i].ToString();
+            }
+
+            return returnString;
+        }
+    }
+}
---
+++ Caeser Shift/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Caeser_Shift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
