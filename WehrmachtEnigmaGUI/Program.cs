a3781d5f88a661c95abcdbf6e4bcbe8f5a0de986
CW0r <charl13worth@gmail.com>
2025-10-29 12:22:06 +0000

    Fullscreen default upon running

--- WehrmachtEnigmaGUI/Program.cs
+++ WehrmachtEnigmaGUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
 namespace WeGUI
@@ -9,8 +10,9 @@ namespace WeGUI
         {
             NativeWindowSettings nativeWindowsSettings = new NativeWindowSettings()
             {
-                ClientSize = new Vector2i(1500, 900),
-                Title = "Wehrmacht Enigma Machine"
+                WindowState = WindowState.Maximized,
+                Title = "Wehrmacht Enigma Machine",
+                ClientSize = new Vector2i(1500, 900)
             };
 
             using (Window window = new Window(GameWindowSettings.Default, nativeWindowsSettings))
