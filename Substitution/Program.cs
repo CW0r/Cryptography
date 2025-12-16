4d11c47ffd97dfecde5b0029dc65e49b607ddb2b
CW0r <charl13worth@gmail.com>
2025-12-16 11:42:46 +0000

    Program class for Substitution Cipher

--- Substitution/Program.cs
+++ Substitution/Program.cs
@@ -6,7 +6,33 @@ namespace Substitution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SubstitutionCipher substitutionCipher = new SubstitutionCipher();
+            string? plaintextMessage, cipherKey;
+
+            Console.WriteLine("Would you like to encode or decode a message?\n1 - Encode\n2 - Decode");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("\nInput the message you want to encode:");
+                    plaintextMessage = Console.ReadLine();
+                    Console.WriteLine("\nInput the cipher key");
+                    cipherKey = Console.ReadLine();
+                    string encodedMessage = substitutionCipher.EncodeMessage(plaintextMessage.ToUpper(), cipherKey.ToUpper());
+                    Console.WriteLine($"\n\nThe encoded message is: {encodedMessage}");
+                    break;
+                case 2:
+                    Console.WriteLine("\nInput the message you want to decode:");
+                    encodedMessage = Console.ReadLine();
+                    Console.WriteLine("\nInput the cipher key");
+                    cipherKey = Console.ReadLine();
+                    plaintextMessage = substitutionCipher.DecodeMessage(encodedMessage.ToUpper(), cipherKey.ToUpper());
+                    Console.WriteLine($"\n\nThe plaintext message is: {plaintextMessage}");
+                    break;
+                default:
+                    Console.WriteLine("I'm sorry, that's not an option");
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
