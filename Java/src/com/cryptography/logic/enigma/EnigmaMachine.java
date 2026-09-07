06c079f7cd24d925cc18fe0a73ad517f0a1fa75a
CW0r <charl13worth@gmail.com>
2026-09-07 16:12:27 +0100

    Base EnigmaMachine abstract class

---
+++ Java/src/com/cryptography/logic/enigma/EnigmaMachine.java
@@ -0,0 +1,71 @@
+package com.cryptography.logic.enigma;
+
+import com.cryptography.logic.Constants;
+import com.cryptography.logic.Tuple2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public abstract class EnigmaMachine {
+    protected ArrayList<String> _rotors = new ArrayList<>();
+    protected String _reflectorPlate;
+    protected HashMap<Character, Character> _plugboardConnections;
+    protected Integer[] _rotorPositions;
+    protected Integer[] _rotorNotchPositions;
+
+    protected void changeMachineSettings(
+            String reflectorSet,
+            ArrayList<Tuple2<Character, Character>> plugboardSettings,
+            Integer[] rotorStartPositions
+    )
+    {
+        _reflectorPlate = reflectorSet;
+        _plugboardConnections = createPlugboard(plugboardSettings);
+        _rotorPositions = rotorStartPositions;
+    }
+
+    protected HashMap<Character, Character> createPlugboard(ArrayList<Tuple2<Character, Character>> plugboardSettings) {
+        HashMap<Character, Character> plugboard = new HashMap<>();
+
+        for (char letter : Constants.AlphabetList) {
+            plugboard.put(letter, letter);
+        }
+
+        for (int i = 0; i < plugboardSettings.size(); i++) {
+            char a = plugboardSettings.get(i).Item1();
+            char b = plugboardSettings.get(i).Item2();
+            plugboard.replace(a, b);
+            plugboard.replace(b, a);
+        }
+
+        return plugboard;
+    }
+
+    protected void rotateRotors() {
+        _rotorPositions[0] = (_rotorPositions[0] + 1) % 26;
+        if (!_rotorPositions[0].equals(_rotorNotchPositions[0])) return;
+
+        _rotorPositions[1] = (_rotorPositions[1] + 1) % 26;
+        if (!_rotorPositions[1].equals(_rotorNotchPositions[1])) return;
+
+        _rotorPositions[2] = (_rotorPositions[2] + 1) % 26;
+    }
+
+    public String encryptMessage(String message) {
+        ArrayList<Character> encryptedCharacters = new ArrayList<>();
+        StringBuilder encryptedMessage = new StringBuilder();
+
+        for (char letter : message.toCharArray()) {
+            if (Constants.AlphabetList.contains(letter)) {
+                char encryptedLetter = encryptLetter(letter);
+                encryptedMessage.append(encryptedLetter);
+            }
+            else
+                encryptedMessage.append(letter);
+        }
+
+        return encryptedMessage.toString();
+    }
+
+    protected abstract char encryptLetter(char letter);
+};
