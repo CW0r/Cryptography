708d4d6a685131ee342e87ac03b647b42d068282
CW0r <charl13worth@gmail.com>
2026-09-07 16:12:52 +0100

    ArmyEnigma implementation

---
+++ Java/src/com/cryptography/logic/enigma/army/ArmyEnigma.java
@@ -0,0 +1,60 @@
+package com.cryptography.logic.enigma.army;
+
+import com.cryptography.logic.Constants;
+import com.cryptography.logic.Tuple2;
+import com.cryptography.logic.enigma.EnigmaMachine;
+
+import java.util.ArrayList;
+
+public class ArmyEnigma extends EnigmaMachine {
+    public final String[] RotorSets = {
+            "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+            "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+            "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+            "ESOVPZJAYQUIRHXLNFTGKDCMWB",
+            "VZBRGITYUPSDNHLXAWMJQOFECK"
+    };
+    public final String[] ReflectorPlateSets = {
+            "EJMZALYXVBWFCRQUONTSPIKHGD",
+            "YRUHQSLDPXNGOKMIEBFZCWVJAT",
+            "FVPJIAOYEDRZXWGCTKUQSBNMHL"
+    };
+
+    public void changeMachineSettings(
+            int chosenReflector,
+            ArrayList<Tuple2<Character, Character>> plugboardSettings,
+            Integer[] rotorStartPositions,
+            Integer[] chosenMovingRotors
+    )
+    {
+        super.changeMachineSettings(ReflectorPlateSets[chosenReflector], plugboardSettings, rotorStartPositions);
+
+        for (int i : chosenMovingRotors)
+            _rotors.add(RotorSets[i]);
+    }
+
+    @Override
+    protected char encryptLetter(char letter) {
+        letter = _plugboardConnections.get(letter);
+
+        for (int i = 0; i < _rotors.size(); i++) {
+            String currentRotor = _rotors.get(i);
+            int index = (Constants.AlphabetList.indexOf(letter) + _rotorPositions[i]) % 26;
+            letter = currentRotor.charAt(index);
+        }
+
+        letter = _reflectorPlate.charAt(Constants.AlphabetList.indexOf(letter));
+
+        for (int i = _rotors.size() - 1; i > -1; i--) {
+            String currentRotor = _rotors.get(i);
+            int index = ((currentRotor.indexOf(letter) - _rotorPositions[i]) + 26) % 26;
+            letter = Constants.AlphabetString.charAt(index);
+        }
+
+        letter = _plugboardConnections.get(letter);
+
+        rotateRotors();
+
+        return letter;
+    }
+}
