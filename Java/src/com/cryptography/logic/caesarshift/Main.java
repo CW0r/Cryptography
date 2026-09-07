94d886a303fc2f7ee48d3e80786813e4692fb5e8
CW0r <charl13worth@gmail.com>
2026-09-07 16:11:11 +0100

    Renamed caesarshift package

---
+++ Java/src/com/cryptography/logic/caesarshift/Main.java
@@ -0,0 +1,9 @@
+package com.cryptography.logic.caesarshift;
+
+public class Main {
+    static void main() {
+        System.out.println(
+                CaesarCipher.encodingShift("Hello", 0)
+        );
+    }
+}
--- Java/src/com/cryptography/logic/casaershift/Main.java
+++
@@ -1,7 +0,0 @@
-package com.cryptography.logic.casaershift;
-
-public class Main {
-    static void main() {
-        System.out.println("Casaer Shift");
-    }
-}
