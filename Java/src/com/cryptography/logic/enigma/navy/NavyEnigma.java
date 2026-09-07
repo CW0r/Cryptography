a93ee275457d7c2c72da24121530c950b6ee7c72
CW0r <charl13worth@gmail.com>
2026-09-07 16:13:03 +0100

    NavyEnigma implementation

---
+++ Java/src/com/cryptography/logic/enigma/navy/NavyEnigma.java
@@ -0,0 +1,80 @@
+package com.cryptography.logic.enigma.navy;
+
+import com.cryptography.logic.Constants;
+import com.cryptography.logic.Tuple2;
+import com.cryptography.logic.enigma.EnigmaMachine;
+
+import java.util.ArrayList;
+
+public class NavyEnigma extends EnigmaMachine {
+    public final String[] MovingRotorSets = {
+            "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+            "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+            "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+            "ESOVPZJAYQUIRHXLNFTGKDCMWB",
+            "VZBRGITYUPSDNHLXAWMJQOFECK",
+            "JPGVOUMFYQBENHZRDKASXLICTW",
+            "NZJHGRCXMYSWBOUFAIVLPEKQDT",
+            "FKQHTLXOCBJSPDZRAMEWNIUYGV"
+    };
+    public final String[] StaticRotorSets = {
+            "LEYJVCNIXWPBQMDRTAKZGFUHOS",
+            "FSOKANUERHMBTIYCWLQPZXVGJD"
+    };
+    public final String[] ReflectorPlateSets = {
+            "EJMZALYXVBWFCRQUONTSPIKHGD",
+            "YRUHQSLDPXNGOKMIEBFZCWVJAT",
+            "FVPJIAOYEDRZXWGCTKUQSBNMHL",
+            "ENKQAUYWJICOPBLMDXZVFTHRGS",
+            "RDOBJNTKVEHMLFCWZAXGYIPSUQ"
+    };
+    private String _staticRotor;
+    private int _staticRotorPosition;
+
+    public void changeMachineSettings(
+            int chosenReflector,
+            ArrayList<Tuple2<Character, Character>> plugboardSettings,
+            Integer[] movingRotorStartPositions,
+            Integer[] chosenMovingRotors,
+            int chosenStaticRotor,
+            int staticRotorStartPosition
+    )
+    {
+        super.changeMachineSettings(ReflectorPlateSets[chosenReflector], plugboardSettings, movingRotorStartPositions);
+
+        for (int i : chosenMovingRotors)
+            _rotors.add(MovingRotorSets[i]);
+
+        _staticRotor = StaticRotorSets[chosenStaticRotor];
+        _staticRotorPosition = staticRotorStartPosition;
+    }
+
+    @Override
+    protected char encryptLetter(char letter) {
+        letter = _plugboardConnections.get(letter);
+
+        for (int i = 0; i < _rotors.size(); i++) {
+            String currentRotor = _rotors.get(i);
+            int index = (Constants.AlphabetList.indexOf(letter) + _rotorPositions[i]) % 26;
+            letter = currentRotor.charAt(index);
+        }
+
+        letter = _staticRotor.charAt((Constants.AlphabetList.indexOf(letter) + _staticRotorPosition) % 26);
+
+        letter = _reflectorPlate.charAt(Constants.AlphabetList.indexOf(letter));
+
+        letter = Constants.AlphabetList.get((_staticRotor.indexOf(letter) - _staticRotorPosition + 26) % 26);
+
+        for (int i = _rotors.size() - 1; i > -1; i--) {
+            String currentRotor = _rotors.get(i);
+            int index = ((currentRotor.indexOf(letter) - _rotorPositions[i]) + 26) % 26;
+            letter = Constants.AlphabetString.charAt(index);
+        }
+
+        letter = _plugboardConnections.get(letter);
+
+        rotateRotors();
+
+        return letter;
+    }
+}
